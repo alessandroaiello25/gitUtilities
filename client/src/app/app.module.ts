f5fd17a30fd29638cafc0e1f9dd6040e28ee93de
Alessandro Aiello <alessandro12.aiello@emeal.nttdata.com>
2025-03-17 14:53:56 +0100

    fix

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';           // For ngModel
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module'; // Routing
 import { AppComponent } from './app.component';
@@ -10,20 +10,14 @@ import { InsertCredentialComponent } from './insert-credential/insert-credential
 import { CredentialListComponent } from './credential-list/credential-list.component';
 import { UpdateCredentialComponent } from './update-credential/update-credential.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    InsertCredentialComponent,
-    CredentialListComponent,
-    UpdateCredentialComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,          // Required for ngModel
-    HttpClientModule,     // Required for HTTP
-    AppRoutingModule      // Provides routerLink, router-outlet
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        InsertCredentialComponent,
+        CredentialListComponent,
+        UpdateCredentialComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule, // Required for HTTP
+        AppRoutingModule // Provides routerLink, router-outlet
+    ], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
