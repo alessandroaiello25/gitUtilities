63329a40516b0655f58ad4a4e811e88c55db6aaf
Alessandro Aiello <alessandro.aiello25194@gmail.com>
2025-03-18 01:51:29 +0100

    fix installer

--- client/main.js
+++ client/main.js
@@ -10,16 +10,7 @@ app.whenReady().then(() => {
 
   const appPath = `file://${path.join(__dirname, 'dist', 'client',  'browser', 'index.html')}`;
   win.loadURL(appPath);
-
-  // Start the Node.js server
-  nodeProcess = spawn('node', ['server.js'], { stdio: 'inherit' });
-
-  win.on('closed', () => {
-    if (nodeProcess) {
-      nodeProcess.kill(); // Kill Node.js process when Electron is closed
-    }
-    app.quit(); // Ensure Electron fully exits
-  });
+  
 });
 
 app.on('window-all-closed', () => {
